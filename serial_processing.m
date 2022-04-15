e6a2f79569a0d0e54a01e744458e7323417d2c19
Michaela Alarie <88394732+michaela-alarie@users.noreply.github.com>
2022-04-15 15:01:48 -0400

    added code that does the serial processing while loops

--- serial_processing.m
+++ serial_processing.m
@@ -4,28 +4,58 @@
 % Updated: April 11, 2022
 clc; clear; close all
 
-%% Download Data File and Create RT, Correct, and trial type Variables
-Data=readmatrix('hier_sub301_sess1_m11_d02_h11_m36.csv');
-Trial_Type=Data(:,5);
-RT2=Data(:,11);
-Correct2=Data(:,12);
+% %% Download Data File and Create RT, Correct, and trial type Variables
+% Data=readmatrix('hier_sub301_sess1_m11_d02_h11_m36.csv');
+% Trial_Type=Data(:,5);
+% RT2=Data(:,11);
+% Correct2=Data(:,12);
+% 
+% %% Remove any trial type 9 values
+% p=1;
+% for k=1:length(Trial_Type)
+%     if Trial_Type(k)==9
+%     else
+%         RT(p,:)=RT2(k);
+%         Correct(p,:)=Correct2(k);
+%         p=p+1;
+%     end
+% end
 
-%% Remove any trial type 9 values
-p=1;
-for k=1:length(Trial_Type)
-    if Trial_Type(k)==9
-    else
-        RT(p,:)=RT2(k);
-        Correct(p,:)=Correct2(k);
-        p=p+1;
-    end
-end
-
-%% Define Parameters
+%% Define Parameters for Model
 A=1; %drift rate
 c=1; %noise
-dt=0.01; % time step
-y0=0; %starting point
+dt=0.001; % time step
+y01=0; %starting point
 N=1000; %num trials
-z=0.5; %threshold
-T0=0.25; %non-decision time
+z1=0.5; %threshold
+z2=0.9; %threshold for stimulus 2
+T0=0.01; %non-decision time
+
+%% Determine Accuracy and RT
+i=1;
+while  abs(y01(i))<z1
+    r=randn(1);
+    dW=sqrt(dt)*r;
+    dy=A*dt+c*dW;
+    y01(i+1)=y01(i)+dy;
+    i=i+1;
+end
+
+y02=y01(end); %starting point for second stimulus
+j=1;
+while  abs(y02(j))<z2
+    r=randn(1);
+    dW2=sqrt(dt)*r;
+    dy2=A*dt+c*dW2;
+    y02(j+1)=y02(j)+dy2;
+    j=j+1;
+end
+
+y=[y01 y02];
+RT=length(y)*dt+T0; 
+correct=0; incorrect=0;
+if y(end)>z2
+    correct=correct+1;
+elseif y(end)<-z2
+    incorrect=incorrect+1;
+end
