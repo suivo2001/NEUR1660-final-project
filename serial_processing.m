79b7fd27f9357e8caa09e0155c96494f8863dd8a
Michaela Alarie <88394732+michaela-alarie@users.noreply.github.com>
2022-04-15 15:17:57 -0400

    runs over many simulations/trials

--- serial_processing.m
+++ serial_processing.m
@@ -1,25 +1,8 @@
 %% Serial Processing DDM
 % Michaela Alarie and Elizabeth Doss
 % Neur1660 Final Project
-% Updated: April 11, 2022
+% Updated: April 15, 2022
 clc; clear; close all
-
-% %% Download Data File and Create RT, Correct, and trial type Variables
-% Data=readmatrix('hier_sub301_sess1_m11_d02_h11_m36.csv');
-% Trial_Type=Data(:,5);
-% RT2=Data(:,11);
-% Correct2=Data(:,12);
-% 
-% %% Remove any trial type 9 values
-% p=1;
-% for k=1:length(Trial_Type)
-%     if Trial_Type(k)==9
-%     else
-%         RT(p,:)=RT2(k);
-%         Correct(p,:)=Correct2(k);
-%         p=p+1;
-%     end
-% end
 
 %% Define Parameters for Model
 A=1; %drift rate
@@ -32,30 +15,38 @@ z2=0.9; %threshold for stimulus 2
 T0=0.01; %non-decision time
 
 %% Determine Accuracy and RT
-i=1;
-while  abs(y01(i))<z1
-    r=randn(1);
-    dW=sqrt(dt)*r;
-    dy=A*dt+c*dW;
-    y01(i+1)=y01(i)+dy;
-    i=i+1;
+correct=zeros([1 N]);
+incorrect=zeros([1 N]);
+RT=zeros([1 N]);
+for f=1:N
+    clear y;
+    i=1;
+    while  abs(y01(i))<z1
+        r=randn(1);
+        dW=sqrt(dt)*r;
+        dy=A*dt+c*dW;
+        y01(i+1)=y01(i)+dy;
+        i=i+1;
+    end
+    y02=y01(end); %starting point for second stimulus
+    j=1;
+    while  abs(y02(j))<z2
+        r=randn(1);
+        dW2=sqrt(dt)*r;
+        dy2=A*dt+c*dW2;
+        y02(j+1)=y02(j)+dy2;
+        j=j+1;
+    end
+    
+    y=[y01 y02];
+    RT(f)=length(y)*dt+T0; 
+    correct(f)=0; incorrect(f)=0;
+    if y(end)>z2
+        correct(f)=correct(f)+1;
+    elseif y(end)<-z2
+        incorrect(f)=incorrect(f)+1;
+    end
 end
 
-y02=y01(end); %starting point for second stimulus
-j=1;
-while  abs(y02(j))<z2
-    r=randn(1);
-    dW2=sqrt(dt)*r;
-    dy2=A*dt+c*dW2;
-    y02(j+1)=y02(j)+dy2;
-    j=j+1;
-end
-
-y=[y01 y02];
-RT=length(y)*dt+T0; 
-correct=0; incorrect=0;
-if y(end)>z2
-    correct=correct+1;
-elseif y(end)<-z2
-    incorrect=incorrect+1;
-end
+percent_correct=mean(correct)*100;
+avg_RT=mean(RT);
