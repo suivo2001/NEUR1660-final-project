f0746163dd4374cf13b3550b46084f9c45d2e5c0
Michaela Alarie <88394732+michaela-alarie@users.noreply.github.com>
2022-04-26 15:32:51 -0400

    testing parameter update
    --updated threshold testing to have a heat map testing all possible threshold combinations for z1 and z2
    --added timestep testing

--- serial_processing/threshold_testing.m
+++ serial_processing/threshold_testing.m
@@ -1,58 +1,61 @@
 %% Define Parameters for Model
 clear; clc; close all;
-%%% Set
 A=1; %drift rate
 c=1; %noise
-N=1000; %num trials
+N=200; %num trials
 y01=0; %starting point
-z2=0.9; %threshold for stimulus 2
 T0=0.01; %non-decision time
-dt=0.01; % time step
+dt=0.05; % time step
 
 %%% Alter
-z1=0.1:0.05:0.8; %threshold
+z1=0.1:0.05:0.85;
+z2=0.15:0.05:0.9;
 
 for k=1:length(z1)
-    [percent_correct(k), percent_falsepositive(k), avg_RT(k)] = SP_multiple_simulations(A,c,dt,y01,z1(k),z2,T0,N);
+    for p=1:length(z2)
+        [percent_correct(k,p), percent_falsepositive(k,p), avg_RT(k,p)] = SP_multiple_simulations(A,c,dt,y01,z1(k),z2(p),T0,N);
+    end
 end
-
-figure(1)
-subplot(211)
-plot(z1,avg_RT, 'o')
-ylabel('Reaction Time (s)')
-subplot(212)
-plot(z1,percent_correct, 'o')
+%%
+figure(3);
 hold on
-plot(z1,percent_correct-percent_falsepositive, 'o', 'Color','r')
-legend('% Correct (Total)', "% Correct (Adjusted)", 'Location', 'northwest')
-ylabel('Accuracy (%)')
-xlabel('Threshold')
-sgtitle('Stimulus 1 Threshold Impacts on Accuracy and RT')
+imagesc(z1, z2, avg_RT);
+xlim([0.1, 0.85])
+ylim([0.15 0.9])
+colorbar
+xlabel('z1')
+ylabel('z2')
+hold off
+sgtitle('Threshold Impacts on RT')
 
-
-%% Change z2
-clear
-z1=0.3;
-A=1; %drift rate
-c=1; %noise
-N=1000; %num trials
-y01=0; %starting point
-T0=0.01; %non-decision time
-dt=0.01; % time step
-z2=0.4:0.05:0.95;
-for k=1:length(z2)
-    [percent_correct(k), percent_falsepositive(k), avg_RT(k)] = SP_multiple_simulations(A,c,dt,y01,z1,z2(k),T0,N);
-end
-
-figure(2)
-subplot(211)
-plot(z2,avg_RT, 'o')
-ylabel('Reaction Time (s)')
-subplot(212)
-plot(z2, percent_correct, 'o')
+%%
+figure(4);
+subplot(131)
 hold on
-plot(z2,percent_correct-percent_falsepositive, 'o', 'Color','r')
-legend('% Correct (Total)', "% Correct (Adjusted)", 'Location', 'northwest')
-ylabel('Accuracy (%)')
-xlabel('Threshold')
-sgtitle('Stimulus 2 Threshold Impacts on Accuracy and RT')
+imagesc(z1, z2, percent_falsepositive);
+xlim([0.1, 0.85])
+ylim([0.15 0.9])
+colorbar
+title('% Correct (FALSE +)')
+xlabel('z1')
+ylabel('z2')
+subplot(132)
+hold on
+imagesc(z1, z2, percent_correct-percent_falsepositive);
+xlim([0.1, 0.85])
+ylim([0.15 0.9])
+colorbar
+title('% Correct (Adjusted)')
+xlabel('z1')
+ylabel('z2')
+subplot(133)
+hold on
+imagesc(z1, z2, percent_correct)%-percent_falsepositive);
+xlim([0.1, 0.85])
+ylim([0.15 0.9])
+colorbar
+title('% Correct (Total)')
+xlabel('z1')
+ylabel('z2')
+hold off
+sgtitle('Threshold Impact on False Positives')
