ee46e7e3c2133378d805e3e0cc4da9d667600ce4
Michaela Alarie <88394732+michaela-alarie@users.noreply.github.com>
2022-04-26 14:16:23 -0400

    updates to serial processing
    --fixed correct computation
    --added optional plotting (commented out in serial_processing.m)
    --adjusted threshold testing to include false positive representation

--- serial_processing/SP_multiple_simulations.m
+++ serial_processing/SP_multiple_simulations.m
@@ -1,4 +1,4 @@
-function [percent_correct, avg_RT] = SP_multiple_simulations(A,c,dt,y01,z1,z2,T0,N)
+function [percent_correct, percent_falsepositive, avg_RT] = SP_multiple_simulations(A,c,dt,y01,z1,z2,T0,N)
 %% Serial Processing (SP) run on multiple simulations (N)
 % NEUR 1660: Final Project
 % Last Update: April 17, 2022
@@ -23,10 +23,12 @@ correct=zeros([1 N]);
 incorrect=zeros([1 N]);
 RT=zeros([1 N]);
 for f=1:N
-    [RT(f), correct(f), incorrect(f)] = serial_processing(A,c,dt,y01,z1,z2,T0);
+    [RT(f), correct(f), incorrect(f), false_positive(f)] = serial_processing(A,c,dt,y01,z1,z2,T0);
     clear y; clear y01; clear y02;
     y01=0;
 end
 
 percent_correct=mean(correct)*100;
+percent_falsepositive=mean(false_positive)*100;
 avg_RT=mean(RT);
+end
--- serial_processing/threshold_testing.m
+++ serial_processing/threshold_testing.m
@@ -1,5 +1,5 @@
-clc; clear; close all;
 %% Define Parameters for Model
+clear; clc; close all;
 %%% Set
 A=1; %drift rate
 c=1; %noise
@@ -13,7 +13,7 @@ dt=0.01; % time step
 z1=0.1:0.05:0.8; %threshold
 
 for k=1:length(z1)
-    [percent_correct(k), avg_RT(k)] = SP_multiple_simulations(A,c,dt,y01,z1(k),z2,T0,N);
+    [percent_correct(k), percent_falsepositive(k), avg_RT(k)] = SP_multiple_simulations(A,c,dt,y01,z1(k),z2,T0,N);
 end
 
 figure(1)
@@ -22,23 +22,37 @@ plot(z1,avg_RT, 'o')
 ylabel('Reaction Time (s)')
 subplot(212)
 plot(z1,percent_correct, 'o')
+hold on
+plot(z1,percent_correct-percent_falsepositive, 'o', 'Color','r')
+legend('% Correct (Total)', "% Correct (Adjusted)", 'Location', 'northwest')
 ylabel('Accuracy (%)')
 xlabel('Threshold')
 sgtitle('Stimulus 1 Threshold Impacts on Accuracy and RT')
 
+
 %% Change z2
+clear
 z1=0.3;
+A=1; %drift rate
+c=1; %noise
+N=1000; %num trials
+y01=0; %starting point
+T0=0.01; %non-decision time
+dt=0.01; % time step
 z2=0.4:0.05:0.95;
 for k=1:length(z2)
-    [percent_correct2(k), avg_RT2(k)] = SP_multiple_simulations(A,c,dt,y01,z1,z2(k),T0,N);
+    [percent_correct(k), percent_falsepositive(k), avg_RT(k)] = SP_multiple_simulations(A,c,dt,y01,z1,z2(k),T0,N);
 end
 
 figure(2)
 subplot(211)
-plot(z2,avg_RT2, 'o')
+plot(z2,avg_RT, 'o')
 ylabel('Reaction Time (s)')
 subplot(212)
-plot(z2,percent_correct2, 'o')
+plot(z2, percent_correct, 'o')
+hold on
+plot(z2,percent_correct-percent_falsepositive, 'o', 'Color','r')
+legend('% Correct (Total)', "% Correct (Adjusted)", 'Location', 'northwest')
 ylabel('Accuracy (%)')
 xlabel('Threshold')
 sgtitle('Stimulus 2 Threshold Impacts on Accuracy and RT')
