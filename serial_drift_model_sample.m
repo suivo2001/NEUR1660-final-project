ac0c4a23ef75c59d9652b5b3b7f6b7e9d1c1e7f9
Elizabeth Doss <52799428+edoss00@users.noreply.github.com>
2022-04-28 02:07:46 -0400

    matt slight modification to serial thresholding

--- serial_drift_model_sample.m
+++ serial_drift_model_sample.m
@@ -44,6 +44,51 @@ ylabel('Evidence Accumulation')
 xlabel('Time Step')
 
 
+%% Try the same thing with different threshold meanings
+%%% So starting point for z2 will be 0
+c=1; %noise
+N=1000; %num trials
+y01=0; %starting point
+z1=0.4; %threshold
+z2=0.9; %threshold for stimulus 2
+T0=0.01; %non-decision time
+dt=0.01; % time step
+A = 0.5; % drift rate
+i=1;
+while  abs(y01(i))<z1
+    r=randn(1);
+    dW=sqrt(dt)*r;
+    dy=A*dt+c*dW;
+    y01(i+1)=y01(i)+dy;
+    i=i+1;
+end
+y01(i+1) = 0;
+y02=0; %starting point for second stimulus
+
+j=1;
+while  abs(y02(j))<z2
+    r=randn(1);
+    dW2=sqrt(dt)*r;
+    dy2=A*dt+c*dW2;
+    y02(j+1)=y02(j)+dy2;
+    j=j+1;
+end
+
+% concatanate the accumulated evidence to plot
+dtmat = 1:1:(i+j+1);
+alldata = cat(2,y01, y02);
+
+hold on
+plot(dtmat,alldata)
+yline(z1)
+yline(-z1)
+yline(z2)
+yline(-z2)
+xline(i)
+title('Evidence Accumulation for z1=0.4,z2=0.9,dt=0.01')
+ylabel('Evidence Accumulation')
+xlabel('Time Step')
+
 % Display only thresholds that matter
 % Have a label/title that states whether decision was correct or not at
 % each threshold
